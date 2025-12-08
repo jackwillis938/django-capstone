7b69812986da6ee72c12b08026224178941cf762
Jack <jackwillis938@gmail.com>
2025-12-07 22:11:38 -0800

    Added IngredientDelete

--- djangodelights/inventory/urls.py
+++ djangodelights/inventory/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("ingredients-list/", views.InventoryListView.as_view(), name="ingredientslist"),
-    path("purchase-list/", views.PurchaseListView.as_view(), name="purchaselist"),
+    path("ingredients/list/", views.IngredientListView.as_view(), name="ingredientslist"),
+    path("purchase/list/", views.PurchaseListView.as_view(), name="purchaselist"),
+    path("ingredients/delete/<pk>", views.IngredientDeleteView.as_view(), name="deleteingredient")
 ]
--- djangodelights/inventory/views.py
+++ djangodelights/inventory/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from .models import Ingredient, RecipeRequirements, MenuItem, Purchase
 from django.views.generic import ListView
+from django.views.generic.edit import DeleteView
+from django.urls import reverse_lazy
 # Create your views here.
 
-class InventoryListView(ListView):
+class IngredientListView(ListView):
     model = Ingredient
     template_name = "inventory/ingredients_list.html"
 
 class PurchaseListView(ListView):
     model = Purchase
     template_name = "purchase_list.html"
+
+class IngredientDeleteView(DeleteView):
+    model = Ingredient
+    template_name = "inventory/ingredients_confirm_delete.html"
+    success_url = reverse_lazy('ingredientslist')
